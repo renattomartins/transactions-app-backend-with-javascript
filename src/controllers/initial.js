491167564d1849113a2fc9fb594fb9b1329842a3
Renato Martins <renatto.martins@gmail.com>
2025-09-30 14:12:06 -0300

    Bump version to 1.1.1 🧩

--- src/controllers/initial.js
+++ src/controllers/initial.js
@@ -2,7 +2,7 @@ exports.getInitial = (req, res) => {
   res.json({
     code: 0,
     message: 'Welcome to Transaction API',
-    version: '1.0.0',
+    version: '1.1.1',
     resources: {
       account_url: `${req.protocol}://${req.get('host')}/accounts`,
     },
